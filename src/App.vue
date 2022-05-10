21fbdf76387b60ee0ac9d4d88c9eb1f2d1b6b0c4
Shigma <shigma10826@gmail.com>
2022-05-11 06:06:34 +0800

    feat: add basic layout

---
+++ src/App.vue
@@ -0,0 +1,67 @@
+<template>
+  <nav>
+    Satori Chat
+    {{ mode }}
+  </nav>
+  <aside></aside>
+  <Main></Main>
+</template>
+
+<script setup lang="ts">
+
+import { useColorMode } from '@vueuse/core'
+import Main from './components/Main.vue'
+
+const mode = useColorMode({
+  storageKey: 'satori-chat-theme',
+  modes: {
+    'discord-dark': 'discord-dark',
+    'discord-light': 'discord-light',
+  },
+})
+
+if (['dark', 'light'].includes(mode.value)) {
+  mode.value = 'discord-' + mode.value as any
+}
+
+</script>
+
+<style lang="scss">
+
+$nav-height: 3rem;
+$aside-width: 20rem;
+
+nav {
+  height: $nav-height;
+  color: var(--nav-color);
+  background-color: var(--nav-bgcolor);
+  position: relative;
+  display: flex;
+  align-items: center;
+  min-width: 0;
+  width: 100%;
+  line-height: 1.25rem;
+  font-weight: 500;
+}
+
+aside {
+  color: var(--side-color);
+  background-color: var(--side-bgcolor);
+  position: fixed;
+  top: $nav-height;
+  left: 0;
+  bottom: 0;
+  width: $aside-width;
+}
+
+main {
+  color: var(--main-color);
+  background-color: var(--main-bgcolor);
+  position: fixed;
+  top: $nav-height;
+  left: $aside-width;
+  bottom: 0;
+  right: 0;
+}
+
+</style>
---
+++ src/components/Main.vue
@@ -0,0 +1,27 @@
+<template>
+  <main>
+    <div class="card-header">
+    </div>
+    <chat-panel class="sandbox" :messages="messages" @send="sendMessage" #="data">
+      <!-- <chat-message :data="data"></chat-message> -->
+    </chat-panel>
+  </main>
+</template>
+
+<script setup lang="ts">
+
+import { reactive } from 'vue'
+import { ChatPanel } from 'semisigure'
+
+const messages = reactive<string[]>([])
+
+function sendMessage(message: string) {
+  messages.push(message)
+}
+
+</script>
+
+<style scoped>
+
+
+</style>
